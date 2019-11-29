0522dd81073120a11d4bea20f43b4c88d9f207e3
Amaury Martiny <amaury.martiny@protonmail.com>
2019-11-29 18:25:05 +0100

    fix: Export everything

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './balanceTransfer';
+export * from './createSignedTx';
+export * from './createSigningPayload';
+export * from './decodeTx';
 export * from './deriveAddress';
 export * from './generateKeypair';
