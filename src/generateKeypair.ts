b5b9c201890fd38cd417fbd5030d8cb0b856b524
Amaury Martiny <amaury.martiny@protonmail.com>
2019-12-02 18:06:20 +0100

    Use Kusama everywhere

--- src/generateKeypair.ts
+++ src/generateKeypair.ts
@@ -2,6 +2,8 @@ import { Keyring } from '@polkadot/keyring';
 import { KeyringPair as KeyringPairBase } from '@polkadot/keyring/types';
 import { mnemonicGenerate } from '@polkadot/util-crypto';
 
+import { KUSAMA_SS58_FORMAT } from './util/constants';
+
 /**
  * A keyring pair
  */
@@ -12,6 +14,7 @@ export interface KeyringPair extends KeyringPairBase {} // eslint-disable-line @
  */
 export function generateKeypair(): KeyringPair {
   const keyring = new Keyring();
+  keyring.setSS58Format(KUSAMA_SS58_FORMAT);
 
   return keyring.addFromMnemonic(mnemonicGenerate());
 }
