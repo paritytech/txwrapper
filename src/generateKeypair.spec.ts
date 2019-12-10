ac4bc643747d68257d65cb2b7fe6515487745768
Amaury Martiny <amaury.martiny@protonmail.com>
2019-12-10 13:35:45 +0100

    feat: Add importPrivateKey

--- src/generateKeypair.spec.ts
+++ src/generateKeypair.spec.ts
@@ -1,9 +1,22 @@
-import { generateKeypair } from './generateKeypair';
+import { generateKeypair, importPrivateKey } from './generateKeypair';
+
+const PRIVATE_KEY =
+  '0x9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60';
 
 describe('generateKeypair', () => {
   it('should work', () => {
     const keypair = generateKeypair();
 
     expect(keypair.address).toBeDefined();
+  });
+});
+
+describe('importPrivateKey', () => {
+  it('should work', () => {
+    const keypair = importPrivateKey(PRIVATE_KEY);
+
+    expect(keypair.address).toBe(
+      'HSgj13mnepYxuysui2XroHKigftFpQsg1dcSfA9PckdZJW4'
+    );
   });
 });
--- src/generateKeypair.ts
+++ src/generateKeypair.ts
@@ -1,8 +1,12 @@
 import { Keyring } from '@polkadot/keyring';
 import { KeyringPair as KeyringPairBase } from '@polkadot/keyring/types';
+import { hexToU8a } from '@polkadot/util';
 import { mnemonicGenerate } from '@polkadot/util-crypto';
 
 import { KUSAMA_SS58_FORMAT } from './util/constants';
+
+const keyring = new Keyring({ type: 'ed25519' });
+keyring.setSS58Format(KUSAMA_SS58_FORMAT);
 
 /**
  * A keyring pair
@@ -10,11 +14,20 @@ import { KUSAMA_SS58_FORMAT } from './util/constants';
 export interface KeyringPair extends KeyringPairBase {} // eslint-disable-line @typescript-eslint/no-empty-interface
 
 /**
- * Generate a valid public/private keypair offline
+ * Generate a random public/private keypair offline
  */
 export function generateKeypair(): KeyringPair {
-  const keyring = new Keyring();
-  keyring.setSS58Format(KUSAMA_SS58_FORMAT);
-
   return keyring.addFromMnemonic(mnemonicGenerate());
+}
+
+/**
+ * Import a private key and create a KeyringPair
+ * @param privateKey - The private key of the key pair
+ */
+export function importPrivateKey(privateKey: string | Uint8Array): KeyringPair {
+  if (typeof privateKey === 'string') {
+    return keyring.addFromSeed(hexToU8a(privateKey));
+  }
+
+  return keyring.addFromSeed(privateKey);
 }
