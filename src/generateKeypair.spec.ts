2b3179c12c2bdebc0696991d30de2fa144534b50
Amaury Martiny <amaury.martiny@protonmail.com>
2019-12-11 10:30:45 +0100

    Fix tests

--- src/generateKeypair.spec.ts
+++ src/generateKeypair.spec.ts
@@ -1,15 +1,7 @@
-import { generateKeypair, importPrivateKey } from './generateKeypair';
+import { importPrivateKey } from './generateKeypair';
 
 const PRIVATE_KEY =
   '0x9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60';
-
-describe('generateKeypair', () => {
-  it('should work', () => {
-    const keypair = generateKeypair();
-
-    expect(keypair.address).toBeDefined();
-  });
-});
 
 describe('importPrivateKey', () => {
   it('should work', () => {
