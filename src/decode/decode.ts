38505a6f9841a93c41bb8ee41780544ac7071a73
joe petrowski <25483142+joepetrowski@users.noreply.github.com>
2019-12-16 17:52:06 +0100

    fix: Add decodeSigningPayload to decode export (#15)
    * add decodeSigningPayload to decode export
    
    * linting
    
    * linting (again)

--- src/decode/decode.ts
+++ src/decode/decode.ts
@@ -1,32 +1,53 @@
 import { TxInfo, UnsignedTransaction } from '../balanceTransfer';
 import { DecodedSignedTx, decodeSignedTx } from './decodeSignedTx';
+import {
+  DecodedSigningPayload,
+  decodeSigningPayload
+} from './decodeSigningPayload';
 import { decodeUnsignedTx } from './decodeUnsignedTx';
 
 /**
- * Parse the transaction information from an unsigned tx or a signed tx
+ * Parse the transaction information from a signing payload, an unsigned tx, or a signed tx.
  *
- * @param unsignedTx - The data to parse, as an unsigned tx
+ * @param unsignedTx - The data to parse, as an unsigned tx.
  * @param metadataRpc - The SCALE-encoded metadata, as a hex string. Can be
- * retrieved via the RPC call `state_getMetadata`
+ * retrieved via the RPC call `state_getMetadata`.
  */
 export function decode(
   unsignedTx: UnsignedTransaction,
   metadataRpc: string
 ): TxInfo;
 /**
- * Parse the transaction information from an unsigned tx or a signed tx
+ * Parse the transaction information from a signing payload, an unsigned tx, or a signed tx.
  *
- * @param signedTx - The data to parse, as a signed tx hex string
+ * @param signedTx - The data to parse, as a signed tx hex string.
  * @param metadataRpc - The SCALE-encoded metadata, as a hex string. Can be
- * retrieved via the RPC call `state_getMetadata`
+ * retrieved via the RPC call `state_getMetadata`.
  */
 export function decode(signedTx: string, metadataRpc: string): DecodedSignedTx;
+/**
+ * Parse the transaction information from a signing payload, an unsigned tx, or a signed tx.
+ *
+ * @param signingPayload - The data to parse, as a signing payload hex string.
+ * @param metadataRpc - The SCALE-encoded metadata, as a hex string. Can be
+ * retrieved via the RPC call `state_getMetadata`.
+ */
+export function decode(
+  signingPayload: string,
+  metadataRpc: string
+): DecodedSigningPayload;
 export function decode(
   data: string | UnsignedTransaction,
   metadataRpc: string
-): DecodedSignedTx | TxInfo {
+): DecodedSignedTx | TxInfo | DecodedSigningPayload {
   if (typeof data === 'string') {
-    return decodeSignedTx(data, metadataRpc);
+    let decodedInfo: DecodedSigningPayload | DecodedSignedTx;
+    try {
+      decodedInfo = decodeSigningPayload(data, metadataRpc);
+    } catch {
+      decodedInfo = decodeSignedTx(data, metadataRpc);
+    }
+    return decodedInfo;
   }
 
   return decodeUnsignedTx(data, metadataRpc);
