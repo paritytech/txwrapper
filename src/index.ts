c8250012f3e1e0bd16d96e8113c058ecdde5d76d
Amaury Martiny <amaury.martiny@protonmail.com>
2020-03-11 16:20:50 +0100

    fix: Export methods object

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as methods from './methods';
+
 export * from './createSignedTx';
 export * from './createSigningPayload';
 export * from './decode/decode';
@@ -5,3 +7,5 @@ export * from './deriveAddress';
 export * from './getTxHash';
 export * from './importPrivateKey';
 export * from './methods';
+
+export { methods };
