13f41e5122e9408d0df4fb59d3b122de939c837f
Amaury Martiny <amaury.martiny@protonmail.com>
2020-05-11 13:11:56 +0200

    fix: Allow balance transfer value as string

--- src/methods/balances/transfer.spec.ts
+++ src/methods/balances/transfer.spec.ts
@@ -17,4 +17,19 @@ describe('balances::transfer', () => {
       '0x060096074594cccf1cd185fa8a72ceaeefd86648f8d45514f3ce33c31bdd07e4655d30'
     );
   });
+
+  it('should accept big numbers as string', () => {
+    const unsigned = transfer(
+      {
+        ...TEST_METHOD_ARGS.balances.transfer,
+        value: '9007199254740996', // MAX_SAFE_INTEGER + 5
+      },
+      TEST_BASE_TX_INFO
+    );
+
+    testBaseTxInfo(unsigned);
+    expect(unsigned.method).toBe(
+      '0x060096074594cccf1cd185fa8a72ceaeefd86648f8d45514f3ce33c31bdd07e4655d0f04000000000020'
+    );
+  });
 });
