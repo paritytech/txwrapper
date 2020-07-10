6a6bfc646d36d1ac8c6a8fc8341bb9af2c8b59cb
Zeke Mostov <z.mostov@gmail.com>
2020-07-09 19:23:01 -0700

    Remove unused lints

--- src/decode/decodeSignedTx.ts
+++ src/decode/decodeSignedTx.ts
@@ -16,11 +16,6 @@ export type DecodedSignedTx = Omit<
   | 'version'
 >;
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-
 /**
  * Parse the transaction information from a signed transaction offline.
  *
