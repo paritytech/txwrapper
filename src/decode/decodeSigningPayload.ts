f3a933eefedea43f6e4141bb468930c952b6dbbe
emostov <32168567+emostov@users.noreply.github.com>
2020-12-16 12:21:14 -0800

    fix: Silence missleading error message when decoding signed tx

--- src/decode/decodeSigningPayload.ts
+++ src/decode/decodeSigningPayload.ts
@@ -2,6 +2,9 @@
  * @ignore
  */ /** */
 
+import { createTypeUnsafe } from '@polkadot/types/create';
+import { Call, ExtrinsicPayload } from '@polkadot/types/interfaces';
+
 import {
   createMetadata,
   EXTRINSIC_VERSION,
@@ -27,10 +30,32 @@ export function decodeSigningPayload(
 
   registry.setMetadata(createMetadata(registry, metadataRpc));
 
-  const payload = registry.createType('ExtrinsicPayload', signingPayload, {
-    version: EXTRINSIC_VERSION,
-  });
-  const methodCall = registry.createType('Call', payload.method);
+  // We use `createTypeUnsafe` here because it allows us to specify `withoutLog: true`,
+  // which silences an internal error message from polkadot-js. This is helpful in `decode`
+  // which takes in just a string. We determine if the string is a signing payload or a
+  // signed tx by first attempting to decode it as a signing payload with this function.
+  // If that fails we catch, knowing through process of elimination it should be a
+  // signed tx. `withoutLog: true` prevents an alarming error message from bubbling up
+  // to the user when we catch.
+  const payload: ExtrinsicPayload = createTypeUnsafe(
+    registry,
+    'ExtrinsicPayload',
+    [
+      signingPayload,
+      {
+        version: EXTRINSIC_VERSION,
+      },
+    ],
+    { withoutLog: true }
+  );
+  const methodCall: Call = createTypeUnsafe(
+    registry,
+    'Call',
+    [payload.method],
+    {
+      withoutLog: true,
+    }
+  );
   const method = toTxMethod(registry, methodCall, toInt);
 
   return {
