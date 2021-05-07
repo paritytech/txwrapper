16e13dbec6ea1469c687f4143cef2d6888eb8aca
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2021-05-07 07:05:14 -0700

    fix: Bump polkadot-js/api; Remove deprecated createType log option (#438)
    * fix: Bump polkadot-js/api; Remove deprecated createType log option
    
    * Update comment

--- src/decode/decodeSigningPayload.ts
+++ src/decode/decodeSigningPayload.ts
@@ -30,13 +30,10 @@ export function decodeSigningPayload(
 
 	registry.setMetadata(createMetadata(registry, metadataRpc));
 
-	// We use `createTypeUnsafe` here because it allows us to specify `withoutLog: true`,
-	// which silences an internal error message from polkadot-js. This is helpful in `decode`
-	// which takes in just a string. We determine if the string is a signing payload or a
+	// We determine if the string is a signing payload or a
 	// signed tx by first attempting to decode it as a signing payload with this function.
 	// If that fails we catch, knowing through process of elimination it should be a
-	// signed tx. `withoutLog: true` prevents an alarming error message from bubbling up
-	// to the user when we catch.
+	// signed tx.
 	const payload: ExtrinsicPayload = createTypeUnsafe(
 		registry,
 		'ExtrinsicPayload',
@@ -45,17 +42,9 @@ export function decodeSigningPayload(
 			{
 				version: EXTRINSIC_VERSION,
 			},
-		],
-		{ withoutLog: true }
+		]
 	);
-	const methodCall: Call = createTypeUnsafe(
-		registry,
-		'Call',
-		[payload.method],
-		{
-			withoutLog: true,
-		}
-	);
+	const methodCall: Call = createTypeUnsafe(registry, 'Call', [payload.method]);
 	const method = toTxMethod(registry, methodCall, toInt);
 
 	return {
